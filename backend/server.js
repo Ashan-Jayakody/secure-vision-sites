d87716eb7f4147f7909eecf7fa158a355bfa629b
Ashan Jayakody <152908308+Ashan-Jayakody@users.noreply.github.com>
2026-04-15 19:46:46 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -241,7 +241,7 @@ app.get('/api/health', (req, res) => {
 // Serve Vite frontend (dist is at project root)
 app.use(express.static(path.join(__dirname, '../dist')));
 
-app.get('/:any(.*)', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist', 'index.html'));
 });
 
